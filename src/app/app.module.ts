5c2df3baedcc22989d80bb4ab8338771cf9802f8
Ayesha Jownally <sheik.bibi.ayesha.siddiqa.jownally@elca.mu>
2020-05-11 12:26:27 +0400

    init project - design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PostsComponent } from './pages/posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
