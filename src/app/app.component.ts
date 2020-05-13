1b6d4623f5812ef79125cc617777e19d2e31bb1d
Ayesha Jownally <sheik.bibi.ayesha.siddiqa.jownally@elca.mu>
2020-05-13 15:47:50 +0400

    remove error import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { AppState } from 'src/store/models/app-state.model';
-import { Observable } from 'rxjs';
-import { ShoppingItem } from 'src/store/models/shopping-item.models';
-import { AddItemAction, DeleteItemAction, LoadShoppingAction } from 'src/store/actions/shopping.actions';
-import { v4 as uuid } from 'uuid';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
